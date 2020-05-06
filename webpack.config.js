d868a039d48b643bc64da09733826401422c815a
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-06 15:03:16 +0900

    add babel config

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,23 @@ module.exports = {
   },
   devServer: {
     contentBase: outputPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ]
+            }
+          }
+        ]
+      }
+    ]
   }
 };
