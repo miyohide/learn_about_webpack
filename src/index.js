cd2c1b972787afdf53a47d9ff07e2d70c9502be2
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-06 16:17:07 +0900

    update sample program.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,8 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const div_element = document.getElementsByTagName('div')[0];
+const span_element = document.createElement('span');
+span_element.innerHTML = 'this is span';
+div_element.replaceWith(span_element);
