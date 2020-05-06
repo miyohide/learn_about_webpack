323e01e3746a88a2aa99b746259f3ec3f683b5e5
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-06 14:05:28 +0900

    add devServer config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: outputPath,
   },
+  devServer: {
+    contentBase: outputPath
+  }
 };
