25d5cf9a3aa165e026c4302b236c9b521837aee3
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-09 14:21:33 +0900

    use calc.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-import _ from 'lodash';
+import * as calc from "./libs/calc";
 
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
-
-const div_element = document.getElementsByTagName('div')[0];
-const span_element = document.createElement('span');
-span_element.innerHTML = 'this is span';
-div_element.replaceWith(span_element);
+const e = document.getElementById('app')
+e.innerText = `1 + 2 = ${calc.add(1, 2)}`
