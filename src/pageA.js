15801a966507010e1a35d5e57eafe50cb1577042
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-11 19:53:00 +0900

    use jquery.

--- src/pageA.js
+++ src/pageA.js
@@ -1,6 +1,10 @@
 import * as logger from './libs/logger'
 import * as calc from './libs/calc'
+import $ from 'jquery'
 
+const e = $('#app')
+e.text(`2 + 3 = ${calc.add(2, 3)}`)
 logger.debug('Message', 'pageA')
-let e = document.getElementById('app')
-e.innerText = `2 + 3 = ${calc.add(2, 3)}`
+for (let i = 0; i < 100; i++) {
+ e.fadeToggle(1000)
+}
