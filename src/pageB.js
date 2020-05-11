ef887144005cb737b2711e83de19a476fc7cfe9c
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-11 19:45:53 +0900

    use fadeToggle.

--- src/pageB.js
+++ src/pageB.js
@@ -1,4 +1,8 @@
-import * as calc from "./libs/calc";
+import * as calc from "./libs/calc"
+import $ from 'jquery'
 
-const e = document.getElementById('app')
-e.innerText = `1 + 2 = ${calc.add(1, 2)}`
+const e = $('#app')
+e.text(`1 + 2 = ${calc.add(1, 2)}`)
+for (let i = 0; i < 100; i++) {
+ e.fadeToggle(500)
+}
