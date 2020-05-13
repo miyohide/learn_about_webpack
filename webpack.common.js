9a0405e731b40b597b137bda811f7778ee46ef8f
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-13 20:36:04 +0900

    add common webpack config

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const glob = require('glob');
+const outputPath = path.resolve(__dirname, 'dist');
+var entries = {};
+
+glob.sync('./src/*.js').forEach(v => {
+  let key = v.replace('./src/', '');
+  entries[key] = v;
+});
+
+module.exports = {
+  entry: entries,
+  output: {
+    filename: '[name]',
+    path: outputPath,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                ]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor.js',
+      chunks: 'initial'
+    }
+  }
+};
