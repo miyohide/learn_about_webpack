8f4ae52bfb6027b7cf54cc6664c42684ba2d09d2
Hidenori Miyoshi <miyohide@gmail.com>
2020-05-14 18:12:28 +0900

    add src/html to contentBase

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,6 +7,6 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: outputPath
+    contentBase: [path.join(__dirname, 'src', 'html'), outputPath]
   }
 })
